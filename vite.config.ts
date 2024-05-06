76389964d1e549f17606b50a8905de670837f196
nishitnagpal <nshtngpl@gmail.com>
2024-05-06 03:37:32 +0200

    Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+    base: "/shoppingcart/",
     plugins: [react()],
     test: {
         environment: 'jsdom',
         setupFiles: './setupTests.ts',
     },
-})
+});
